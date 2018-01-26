c972e956f0775ec7c176550073c3d33f53e54cdb
123 <a123@123demac-mini.local>
2018-01-26 15:31:19 +0800

    添加请求文件

---
+++ WenDaoGeomancy/请求/SLRequest.swift
@@ -0,0 +1,13 @@
+//
+//  SLRequest.swift
+//  WenDaoGeomancy
+//
+//  Created by 123 on 2018/1/26.
+//  Copyright © 2018年 handsomeBoy. All rights reserved.
+//
+
+import UIKit
+
+class SLRequest: NSObject {
+
+}
