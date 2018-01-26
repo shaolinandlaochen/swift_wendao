c29347d502da9f1ef21bf513c90435d3c8daa594
123 <a123@123demac-mini.local>
2018-01-26 11:31:49 +0800

    设置tabBar颜色大小图片

--- WenDaoGeomancy/tabBar/TabBarViewController.swift
+++ WenDaoGeomancy/tabBar/TabBarViewController.swift
@@ -24,30 +24,29 @@ class TabBarViewController: UITabBarController {
         home.title = "首页"
         let navHome = UINavigationController(rootViewController:home)
         navHome.tabBarItem.title = "首页"
-        navHome.tabBarItem.image = UIImage(named:"main")
-        navHome.tabBarItem.selectedImage = UIImage(named:"main_blue")
+        navHome.tabBarItem.image = UIImage(named:"icon_index_wendao_nor")
+        navHome.tabBarItem.selectedImage = UIImage(named:"icon_index_wendao_sel")
         
         let  shuoDao  = ShuoDaoViewController()
-        shuoDao.title = "分类"
+        shuoDao.title = "说道"
         let navShuoDao = UINavigationController(rootViewController:shuoDao)
-        navShuoDao.tabBarItem.title = "分类"
-        navShuoDao.tabBarItem.image = UIImage(named:"grid")
-        navShuoDao.tabBarItem.selectedImage = UIImage(named:"grid_blue")
+        navShuoDao.tabBarItem.title = "说道"
+        navShuoDao.tabBarItem.image = UIImage(named:"icon_index_fengshui_nor")
+        navShuoDao.tabBarItem.selectedImage = UIImage(named:"icon_index_fengshui_sel")
         
         let  message  = MessageViewController()
         message.title = "消息"
         let navMessage = UINavigationController(rootViewController:message)
         navMessage.tabBarItem.title = "消息"
-        navMessage.tabBarItem.image = UIImage(named:"grid")
-        navMessage.tabBarItem.selectedImage = UIImage(named:"grid_blue")
+        navMessage.tabBarItem.image = UIImage(named:"icon_index_news_nor")
+        navMessage.tabBarItem.selectedImage = UIImage(named:"icon_index_news_sel")
         
         let  MyView  = MyViewController()
         MyView.title = "我的"
         let navMyView = UINavigationController(rootViewController:MyView)
         navMyView.tabBarItem.title = "我的"
-        navMyView.tabBarItem.image = UIImage(named:"grid")
-        navMyView.tabBarItem.selectedImage = UIImage(named:"grid_blue")
-        
+        navMyView.tabBarItem.image = UIImage(named:"icon_index_mine_nor")
+        navMyView.tabBarItem.selectedImage = UIImage(named:"icon_index_mine_sel")
         self.viewControllers = [navHome, navShuoDao,navMessage,navMyView]
         
 
@@ -65,9 +64,35 @@ class TabBarViewController: UITabBarController {
         self.tabBar.backgroundImage = UIImage(named: "")
         // 4 选中时的背景图片
         self.tabBar.selectionIndicatorImage = UIImage(named: "")
-     
-        //self.tabBar.tintColor = UIColor.colo
+        // 5 设置tabbar字体颜色
+        self.tabBar.tintColor = withHex(hexString: "d9a700", alpha: 1)
+        // 设置选中按钮的颜色
+        self.tabBar.unselectedItemTintColor=withHex(hexString: "000000", alpha: 1)
+        // 6 改变字体大小
+        navHome.tabBarItem.setTitleTextAttributes([NSAttributedStringKey.font : UIFont.systemFont(ofSize: 12)], for: .normal)
+        navShuoDao.tabBarItem.setTitleTextAttributes([NSAttributedStringKey.font : UIFont.systemFont(ofSize: 12)], for: .normal)
+        navMessage.tabBarItem.setTitleTextAttributes([NSAttributedStringKey.font : UIFont.systemFont(ofSize: 12)], for: .normal)
+        navMyView.tabBarItem.setTitleTextAttributes([NSAttributedStringKey.font : UIFont.systemFont(ofSize: 12)], for: .normal)
         
+    }
+    func withHex(hexString hex: String, alpha:CGFloat = 1) -> UIColor {
+        // 去除空格等
+        var cString: String = hex.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines).uppercased()
+        // 去除#
+        if (cString.hasPrefix("#")) {
+            cString = (cString as NSString).substring(from: 1)
+        }
+        // 红色的色值
+        let rString = (cString as NSString).substring(to: 2)
+        let gString = ((cString as NSString).substring(from: 2) as NSString).substring(to: 2)
+        let bString = ((cString as NSString).substring(from: 4) as NSString).substring(to: 2)
+        // 字符串转换
+        var r:CUnsignedInt = 0, g:CUnsignedInt = 0, b:CUnsignedInt = 0
+        Scanner(string: rString).scanHexInt32(&r)
+        Scanner(string: gString).scanHexInt32(&g)
+        Scanner(string: bString).scanHexInt32(&b)
+        
+        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: alpha)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
