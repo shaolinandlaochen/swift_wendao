9512b7e5828c051efc9252a9befa421e26798467
123 <a123@123demac-mini.local>
2018-01-26 14:05:10 +0800

    ....添加表格

--- WenDaoGeomancy/首页/HomeViewController.swift
+++ WenDaoGeomancy/首页/HomeViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super .viewWillAppear(true)
+        self.navigationController?.isNavigationBarHidden=true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor=UIColor.blue
+        self.view.backgroundColor=UIColor.white
+        tableView.delegate=self
+        tableView.dataSource=self
+
 
         // Do any additional setup after loading the view.
+    }
+
+    //MARK: - 懒加载数据
+    lazy var dataList:[String] = {
+        return ["iOScoderZZJ","iOS小菜鸡","终成雄鹰","旅途很远","还需修行"]
+    }()
+    
+    //MARK: 行数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataList.count
+    }
+    //MARK: 点击cell执行
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView .deselectRow(at: indexPath, animated: true)
+        print("点击cell执行")
+    }
+    //加载cell
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil{
+            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+            
+        }
+        cell?.textLabel?.text = dataList[indexPath.row]
+
+        return cell!
     }
 
     @IBAction func pusBtn(_ sender: UIButton) {
